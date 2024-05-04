65e7e267d6df3c2cd51fa41cddbb06620eef8b87
Tom <tomleesm@gmail.com>
2024-05-04 22:18:51 +0800

    Migrating from Vite to Laravel Mix Demo

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+  </body>
+</html>
